f26fdf221a5454c9860ee09529ed652eecc5a21f
James Sully <sullyj3@gmail.com>
2014-02-18 05:12:52 +1100

    now successfully gets a list of colors. Just need to figure out how to display them

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import sys
-
-args=sys.argv[1:]
-num_args=len(sys.argv)-1
 debug=True
+def debug_print(x):
+    if debug==True:
+        print("DEBUG PRINTER")
+        print(x)
+
+arg=sys.argv[1]
+
+debug_print("arg is "+arg)
+
+num_args=len(sys.argv)-1
+color_list=[]
 
 if num_args>1:#can only handle 1 file at a time for now
     print("too many input files")
     sys.exit()
 
-def debug_print(x):
-    if debug==True:
-        print(x)
-    
 def get_colors(input_file):
     for line in input_file:
-        debug_print(line)
         if "#" in line:
-            pass
+            #append hex code to color_list
+            hexcode=line[line.index("#"):line.index("#")+7]
+            #debug_print(hexcode)
+            color_list.append(hexcode)
 
 def print_colors(collist):
     pass
 
-input_file=open(args[1],'r')
+input_file=open(arg,'r')
 
+get_colors(input_file)
+#debug_print(color_list)
 input_file.close()
