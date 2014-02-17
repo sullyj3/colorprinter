8cb7c60831ac2c50b2adbda85b276339a11d1ef2
James Sully <sullyj3@gmail.com>
2014-02-18 04:17:01 +1100

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+args=sys.argv[1:]
+num_args=len(sys.argv)-1
+
+if num_args>1:#can only handle 1 file at a time for now
+    print("too many input files")
+    sys.exit()
+
+def printcols(file):
+    pass
