8863c64cf815ddb0e0371de80a44a966901deb7f
James Sully <sullyj3@gmail.com>
2014-02-18 04:50:32 +1100

    added debug print, added get_colors()

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import sys
+
 args=sys.argv[1:]
 num_args=len(sys.argv)-1
+debug=True
 
 if num_args>1:#can only handle 1 file at a time for now
     print("too many input files")
     sys.exit()
 
-infile=open(args[1],'r')
+def debug_print(x):
+    if debug==True:
+        print(x)
+    
+def get_colors(input_file):
+    for line in input_file:
+        debug_print(line)
+        if "#" in line:
+            pass
 
-def printcols(file):
+def print_colors(collist):
     pass
 
-infile.close()
+input_file=open(args[1],'r')
+
+input_file.close()
